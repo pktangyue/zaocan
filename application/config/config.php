405ecd1124b2a5ac2dafd4acab7c85405a3b13d4
tangyue <tangyue1004@gmail.com>
2012-08-07 17:30:20 +0800

    base config

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'aowe50f49d25982f4ac751a98d51b4e0';
 
 /*
 |--------------------------------------------------------------------------
