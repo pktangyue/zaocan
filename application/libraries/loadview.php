59c11518aa8ed610b39fa3e98eeb8939b970899c
tangyue <tangyue1004@gmail.com>
2012-08-13 22:29:08 +0800

    use mobile as default view

--- application/libraries/loadview.php
+++ application/libraries/loadview.php
@@ -1,10 +1,17 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 class Loadview {
+    public function __construct() {
+        //现在不显示pc，直接exit，到后面做pc版，删除这段代码即可
+        $CI = & get_instance();
+        if (!$CI->agent->is_mobile()) {
+            exit;
+        }
+    }
     public function path($view, $vars = array() , $return = FALSE) {
         $CI = & get_instance();
-        if ($CI->agent->is_mobile()) {
-            $view = 'mobile/' . $view;
+        if (!$CI->agent->is_mobile()) {
+            $view = 'pc/' . $view;
         }
         $CI->parser->parse($view, $vars, $return);
     }
