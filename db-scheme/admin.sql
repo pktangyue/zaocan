56ecdc9645fbe5815e05debe5bb58d1d02b546eb
tangyue <tangyue1004@gmail.com>
2012-08-17 23:12:55 +0800

    add token for autologin

--- db-scheme/admin.sql
+++ db-scheme/admin.sql
@@ -3,6 +3,7 @@ CREATE TABLE `admin` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `name` varchar(16) NOT NULL,
     `password` varchar(32) NOT NULL,
+    `token` varchar(32) DEFAULT NULL,
     `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     UNIQUE KEY `name` (`name`)
