ef0c17a0c408001f0b5660b394ff5bc71e61fa69
tangyue <tangyue1004@gmail.com>
2012-08-18 18:44:41 +0800

    improve design of token

--- db-scheme/user_token.sql
+++ db-scheme/user_token.sql
@@ -2,8 +2,9 @@ USE `zaocan`;
 CREATE TABLE `user_token` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `uid` int(11) NOT NULL,
-    `series` int(11) NOT NULL,
+    `series` varchar(12) NOT NULL,
     `token` varchar(128) NOT NULL,
     `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (`id`)
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `uid_series` (`uid`,`series`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
