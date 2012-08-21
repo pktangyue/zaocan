c797d14119afb3c80d0c3e74e18bebc264102d38
tangyue <tangyue1004@gmail.com>
2012-08-21 20:48:56 +0800

    re-design goods

--- db-scheme/goods.sql
+++ db-scheme/goods.sql
@@ -3,5 +3,8 @@ CREATE TABLE `goods` (
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `name` varchar(32) NOT NULL,
     `price` float(11,2) NOT NULL,
-    PRIMARY KEY (`id`)
+    `is_set` tinyint(1) DEFAULT '0',
+    `is_delete` tinyint(1) DEFAULT '0',
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `name` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
