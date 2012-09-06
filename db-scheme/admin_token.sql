1fce095f1b08f3c93f84f05bb0e5c627c00bf104
tangyue <tangyue1004@gmail.com>
2012-09-06 16:52:48 +0800

    update create_time on update in admin_token and user_token

--- db-scheme/admin_token.sql
+++ db-scheme/admin_token.sql
@@ -4,7 +4,7 @@ CREATE TABLE `admin_token` (
     `uid` int(11) NOT NULL,
     `series` varchar(12) NOT NULL,
     `token` varchar(128) NOT NULL,
-    `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`id`),
     UNIQUE KEY `uid_series` (`uid`,`series`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
